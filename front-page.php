9540280abc00ff94ca55a907f07e4eae02c9718d
JuniorTavarez <Jtavarez8@gmail.com>
2019-07-03 22:46:36 -0400

    updating landing page

--- front-page.php
+++ front-page.php
@@ -140,6 +140,9 @@
          
     <section id="contact-section">
         <div class="container">
+        <div class="large-title">
+          Contact
+        </div>
             <?php echo do_shortcode( '[contact-form-7 id="1234" title="Contact form 1"]' ); ?>
         </div>
     </section>
